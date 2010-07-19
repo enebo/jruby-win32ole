a90339696f96facbaade6b014380acd6a7a98aa0
Thomas E Enebo <tom.enebo@gmail.com>
2010-07-19 11:30:02 -0500

    Add invoke example to sample

--- samples/ie_simple.rb
+++ samples/ie_simple.rb
@@ -3,13 +3,16 @@ require 'win32ole'
 ie = WIN32OLE.new('InternetExplorer.Application')
 puts "VISIBLE: #{ie.visible}"
 puts "VISIBLE w/ []: #{ie['Visible']}"
+puts "VISIBLE w/ Invoke: #{ie.invoke('Visible')}"
 ie.Visible = TRUE  # Upper-case
 puts "VISIBLE: #{ie.visible}"
 sleep 1
 ie['Visible'] = false
 puts "VISIBLE: #{ie.visible}"
+#puts ie.ole_methods
 ie.gohome
 puts "NAME: #{ie.name}"  # Lower-case
 ie.quit 
+
 
 
