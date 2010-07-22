7edb37d1fa0c4821b1665d7607e8d57b9c82b113
Thomas E Enebo <tom.enebo@gmail.com>
2010-07-22 17:44:07 -0500

    Preliminary WIN32OLE_TYPE support

--- lib/win32ole/win32ole_type.rb
+++ lib/win32ole/win32ole_type.rb
@@ -1,7 +1,32 @@
 class WIN32OLE_TYPE
-  def initialize(typelib, ole_class)
+  def initialize(typelib, info, docs)
+    @typelib, @info, @docs = typelib, info, docs
   end
 
-  def ole_type
+  def name
+    @docs.name
+  end
+
+  def major_version
+    @info.major_version
+  end
+
+  def minor_version
+    @info.minor_version
+  end
+
+  def typekind
+    @info.typekind
+  end
+
+  class << self
+    # This is obsolete, but easy to emulate
+    def typelibs
+      WIN32OLE_TYPELIB.typelibs.collect {|t| t.name }
+    end
+
+    def ole_classes(tlib)
+      WIN32OLE_TYPELIB.ole_classes(tlib)
+    end
   end
 end
