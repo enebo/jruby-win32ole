9fdaaaf06d3edd879e8ef485b4157a913d0c3474
Thomas E Enebo <tom.enebo@gmail.com>
2010-09-15 20:15:50 -0500

    Simple script to make a crap load of empty files for dir_enum_bench

---
+++ bin/make_data.rb
@@ -0,0 +1,10 @@
+require 'fileutils'
+
+number_of_files = 1_000
+directory = "c:/opt/test_data"
+
+Dir.mkdir directory unless File.exists? directory
+number_of_files.times do |i|
+  file = directory + "/" + i.to_s
+  FileUtils.touch file
+end
