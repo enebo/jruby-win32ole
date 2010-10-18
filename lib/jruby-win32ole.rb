1c5875decdb0de7332d7da9e1471944c51e08bac
enebo <tom.enebo@gmail.com>
2010-10-18 16:03:39 -0500

    Load racob based on 32/64 vm settings

--- lib/jruby-win32ole.rb
+++ lib/jruby-win32ole.rb
@@ -1,5 +1,11 @@
 require 'java'
-require 'jacob.jar'
+
+# Set location so the loader can find the native dll it needs
+arch = ENV_JAVA['sun.arch.data.model'] == '32' ? 'x86' : 'x64'
+java.lang.System.set_property 'jacob.dll.path', 
+  File.dirname(__FILE__) + "/racob-#{arch}.dll"
+
+require 'racob.jar'
 
 require 'win32ole/win32ole'      # <- java native impl of WIN32OLE
 require 'win32ole/win32ole_ruby' # <- ruby impl of WIN32OLE
